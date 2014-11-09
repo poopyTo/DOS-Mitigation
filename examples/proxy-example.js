41694a4f514f035404428efc31972d0882fbf19a
Amadeo <amadeo.mazzara@gmail.com>
2014-11-09 15:20:29 -0500

    Added some examples for the project

---
+++ examples/proxy-example.js
@@ -0,0 +1,28 @@
+var util = require('util'),
+    colors = require('colors'),
+    http = require('http'),
+    httpProxy = require('node-http-proxy');
+
+//
+// Http Server with proxyRequest Handler and Latency
+//
+var proxy = new httpProxy.createProxyServer();
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    proxy.web(req, res, {
+      target: 'http://localhost:9002'
+    });
+  }, 200);
+}).listen(8002);
+
+//
+// Target Http Server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9002);
+
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '8002 '.yellow + 'with proxy.web() handler'.cyan.underline + ' and latency'.magenta);
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9001 '.yellow);
