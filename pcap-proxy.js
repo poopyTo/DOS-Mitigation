3ce46f81fea085618f54f7b8d0078c34ba1b0c78
poopyTo <chan.mike@hotmail.com>
2014-12-17 14:18:26 -0500

    Basic IP Blocking has been implemented, but SYN-Flood currently still works

--- pcap-proxy.js
+++ pcap-proxy.js
@@ -3,6 +3,7 @@ var util = require('util'),
     http = require('http'),
     httpProxy = require('http-proxy'),
     pcap = require('pcap'),
+    pcap_session = pcap.createSession("", "ip proto \\tcp"),
     fs = require('fs');
 
 //
@@ -11,53 +12,59 @@ var util = require('util'),
 
 var proxy = new httpProxy.createProxyServer();
 
+Array.prototype.contains = function(elem)
+{
+	for (var i in this)
+	{
+		if (this[i]==elem) return true;
+	}
+	return false;
+}
+
 http.createServer(function (req, res) {
+	var ipDrop = new Array();
+	var messages = {};
 
-  var ipDrop = {};
-  
-  ipDrop.on = (function(){
-	  var messages = {};
-	  var speedLimit = 5; //5ms
-	  return function(message, func) {
-		  messages[message] = messages[message] || {};
-		  if (messages[message].timestamp && new Date().getTime() - messages[message].timestamp < speedLimit) {
-			  res.writeHead(200, { 'Content-Type': 'text/plain' });
-			  res.write('Hey, fuck you buddy.');
-			  res.end();
-		  }
-		  else messages[message].timestamp = new Date().getTime();
-		  
-		  setTimeout(func, 200);
-		  return true;
-	  }
-  }());
-    
-  ipDrop.on(req, function () {
-    proxy.web(req, res, {
-      target: 'http://localhost:9002'
-    });
-  });
+	tcp_tracker = new pcap.TCP_tracker(),
 
-  /*
+	tcp_tracker.on('start', function (session) {
+		console.log("Start of TCP session between " + session.src_name + " and " + session.dst_name);
+		JSON.stringify(session,null,2);
+	});
 
-  tcp_tracker = new pcap.TCP_tracker(),
-  pcap_session = pcap.createSession("", "ip proto \\tcp");
+	tcp_tracker.on('end', function (session) {
+		console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
+	});
 
-  tcp_tracker.on('start', function (session) {
-      console.log("Start of TCP session between " + session.src_name + " and " + session.dst_name);
-      JSON.stringify(session,null,2);
-  });
+	pcap_session.on('packet', function (raw_packet) {
+		var packet = pcap.decode.packet(raw_packet);
+		var speedLimit = 5; //5ms
+		messages[packet.link.ip.saddr] = messages[packet.link.ip.saddr] || {};
+		
+		if (messages[packet.link.ip.saddr].timestamp && new Date().getTime() - messages[packet.link.ip.saddr].timestamp < speedLimit)
+			if (!ipDrop.contains(packet.link.ip.saddr)) {
+				ipDrop.push(packet.link.ip.saddr);
+				console.log(packet.link.ip.saddr + ' pushed');
+			}
+		else messages[packet.link.ip.saddr].timestamp = new Date().getTime();
+		
+		if (!ipDrop.contains(packet.link.ip.saddr)) {
+			console.log("Everything is fine for " + packet.link.ip.saddr);
+			tcp_tracker.track_packet(packet);
+		}
+	});
 
-  tcp_tracker.on('end', function (session) {
-      console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
-  });
-
-  pcap_session.on('packet', function (raw_packet) {
-      var packet = pcap.decode.packet(raw_packet);
-      tcp_tracker.track_packet(packet);
-  });
-  */
-
+	setTimeout(function() {
+		if (!ipDrop.contains(req.connection.remoteAddress))
+			proxy.web(req, res, {
+				target: 'http://localhost:9002'
+			});
+		else {
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+			res.write('Hey, we know about the DOS');
+			res.end();
+		}
+	}, 200);
 
 }).listen(8002, "0.0.0.0");
 
@@ -65,9 +72,9 @@ http.createServer(function (req, res) {
 // Target Http Server
 //
 http.createServer(function (req, res) {
-  var img = fs.readFileSync('./Content/images/barbeau.png');
-  res.writeHead(200, { 'Content-Type': 'image/png' });
-  res.end(img, 'binary');
+	var img = fs.readFileSync('./Content/images/barbeau.png');
+	res.writeHead(200, { 'Content-Type': 'image/png' });
+	res.end(img, 'binary');
 }).listen(9002, "0.0.0.0");
 
 util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '8002 '.yellow + 'with proxy.web() handler'.cyan.underline + ' and latency'.magenta);
